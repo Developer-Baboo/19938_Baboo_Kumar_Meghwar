d2c55443e80adef33fe787de1975d17ead807955
Developer-Baboo <babooheerani@gmail.com>
2023-02-21 12:12:50 +0500

    ..........

---
+++ Topic 6 intro to CSS/day_03/1 Web Page Design/19938_Baboo_Kumar_Meghwar.php
@@ -0,0 +1,62 @@
+<!Doctype html>
+<html>
+
+<head>
+    <title>.::WEB PAGE LAYOUT DESIGN::.</title>
+    <link rel="stylesheet" href="./mystyle.css">
+</head>
+
+<body style="background-color: lightblue;font-family:Georgia, 'Times New Roman', Times, serif ">
+    <center><h1 style="background-color:darkgray; padding: 10px; text-shadow: 0 0 10px #FF0000; ">Our Projects</h1>
+    </center>
+    <div class="row">
+        <div style="background-color:aqua" class="col-4">
+            <center>
+                <h2 style="text-shadow: 2px 2px red;">Facebook Login</h2>
+            </center>
+            <p>
+                <img style="border-radius: 10px;" src="./images/facebook.PNG" alt="" height="10%" width="100%">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium odio commodi soluta, molestias ipsum debitis dolorum corporis totam vero, ad sit quae. Id nesciunt, deserunt maxime quidem consequatur dolore magni?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium odio commodi soluta, molestias ipsum debitis dolorum corporis totam vero, ad sit quae. Id nesciunt, deserunt maxime quidem consequatur dolore magni?
+
+            </p>
+        </div>
+
+        <div style="background-color:lightgreen; margin-right:20px; margin-left: 20px;  " class="col-4">
+            <center>
+                <h2 style="color: white;
+  text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue;">Stop Watch</h2>
+            </center>
+            <img style="border-radius: 10px;" src="./images/Capture.PNG" height="27%" width="100%" alt="">
+            <p>
+                Lorem ipsum dolor sit amet conse ctetur, adipi sicing elit. Est bland itiis reici endis volup tatum velit, ab alias aper iam facilis earum neque ullam quae, iusto fugiat, conseq uatur, quidem num quam volup tates iure min ima? Com modi.
+            </p>
+        </div>
+        <div style="background-color:aliceblue" class="col-4">
+            <center>
+                <h2 style="color: white;text-shadow: 2px 2px 4px #000000;">Login Form</h2>
+            </center>
+            <img style="border-radius: 10px;" src="./images/saylani.PNG" height="27%" width="100%" alt="">
+            <p>
+                Lorem ipsum dolor sit amet conse ctetur, adipi sicing elit. Est bland itiis reici endis volup tatum velit, ab alias aper iam facilis earum neque ullam quae, iusto fugiat, conseq uatur, quidem num quam volup tates iure min ima? Com modi.
+            </p>
+        </div>
+        <br>
+
+
+
+    </div>
+    <div style="background-color:burlywood; margin:0px" class="col-12">
+        <center>
+            <footer>
+                <center>
+                    <p>
+                        Copyright Hidaya Institue of Science & Technology
+                    </p>
+                </center>
+            </footer>
+        </center>
+    </div>
+</body>
+
+</html>
---
+++ Topic 6 intro to CSS/day_03/2 30 CSS Properties/19938_Baboo_Kumar_Meghwar.php
@@ -0,0 +1,37 @@
+<!Doctype html>
+<html>
+
+<head>
+    <title>.::WEB PAGE LAYOUT DESIGN::.</title>
+    <link rel="stylesheet" href="./mystyle.css">
+</head>
+
+<body style="background-color: lightblue;font-family:Georgia, 'Times New Roman', Times, serif ">
+
+    <ul class="main-navigation">
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Resource</a></li>
+        <li><a href="#">Inspiration</a></li>
+    </ul>
+    <center>
+        <h1 style="background-color:aquamarine; padding: p9x;">Our Story</h1>
+    </center>
+    <center>
+        <div class="row">
+            <img style="text-align:center" ; src="./images/11.PNG" alt="">
+        </div>
+    </center>
+    <p class="col-12">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, dolore? Enim consequuntur velit et vel consequatur, provident modi repellat cumque nobis maxime ut molestiae, totam rerum vitae itaque in eaque!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, dolore? Enim consequuntur velit et vel consequatur, provident modi repellat cumque nobis maxime ut molestiae, totam rerum vitae itaque in eaque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, dolore? Enim consequuntur velit et vel consequatur, provident modi repellat cumque nobis maxime ut molestiae, totam rerum vitae itaque in eaque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, dolore? Enim consequuntur velit et vel consequatur, provident modi repellat cumque nobis maxime ut molestiae, totam rerum vitae itaque in eaque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, dolore? Enim consequuntur velit et vel consequatur, provident modi repellat cumque nobis maxime ut molestiae, totam rerum vitae itaque in eaque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, dolore? Enim consequuntur velit et vel consequatur, provident modi repellat cumque nobis maxime ut molestiae, totam rerum vitae itaque in eaque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, dolore? Enim consequuntur velit et vel consequatur, provident modi repellat cumque nobis maxime ut molestiae, totam rerum vitae itaque in eaque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, dolore? Enim consequuntur velit et vel consequatur, provident modi repellat cumque nobis maxime ut molestiae, totam rerum vitae itaque in eaque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, dolore? Enim consequuntur velit et vel consequatur, provident modi repellat cumque nobis maxime ut molestiae, totam rerum vitae itaque in eaque!
+    </p>
+    <footer>
+        <center>
+            <p style="background-color:grey;padding:10px">
+                Copyright Developer Baboo Kumar Meghwar id: 19938
+            </p>
+        </center>
+    </footer>
+</body>
+
+</html>
