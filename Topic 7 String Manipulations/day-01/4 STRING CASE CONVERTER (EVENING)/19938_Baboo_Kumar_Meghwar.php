a34dc8cb4921e60cfe2221c3217e75a7fdd6f788
Developer-Baboo <babooheerani@gmail.com>
2023-02-25 08:55:22 +0500

    modified

--- Topic 7 String Manipulations/day-01/4 STRING CASE CONVERTER (EVENING)/19938_Baboo_Kumar_Meghwar.php
+++ Topic 7 String Manipulations/day-01/4 STRING CASE CONVERTER (EVENING)/19938_Baboo_Kumar_Meghwar.php
@@ -1,34 +1,196 @@
-<!Doctype html>
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>STRING VASE CONVERTOR EVENING</title>
+    <title>Assignment</title>
 </head>
 
 <body>
-    <form method="POST">
-        <center>
+    <center>
+        <form action="" method="POST">
             <table>
                 <tr>
-                    <td><label>Enter Text : </label> </td>
-                    <td><input type="text" name="value"></td>
-                    <td><input type="submit" name="submit" value="Go!" /> </td>
+                    <td><label>Enter Text : </label></td>
+                    <td><input type="text" name="value"> </td>
+                    <td><input type="submit" name="submit" value="Go!"></td>
                 </tr>
             </table>
-        </center>
-    </form>
-    <?php
-    if (isset($_POST['submit'])) {
-        $value = $_POST['value'];
-        // check each character in the string via loop
-        for ($a = 0; isset($value[$a]); $a++) {
-            if ($value[$a] === 'A' || $value[$a] === 'a') {
-                
-            }
-        }
-        echo "$value";
-    }
-    ?>
+        </form>
+    </center>
 </body>
 
 </html>
+
+<?php
+$abc1 = " ";
+$abc2 = " ";
+$abc3 = " ";
+$abc4 = " ";
+$abc5 = " ";
+$abc6 = " ";
+$abc7 = " ";
+$abc8 = " ";
+$abc9 = " ";
+$abc10 = " ";
+$abc11 = " ";
+$abc12 = " ";
+$abc13 = " ";
+$abc14 = " ";
+$abc15 = " ";
+$abc16 = " ";
+$abc17 = " ";
+$abc18 = " ";
+$abc19 = " ";
+$abc20 = " ";
+$abc21 = " ";
+$abc22 = " ";
+$abc23 = " ";
+$abc24 = " ";
+$abc25 = " ";
+$abc26 = " ";
+$abc27 = " ";
+$abc28 = " ";
+$abc29 = " ";
+$abc30 = " ";
+$abc31 = " ";
+$abc32 = " ";
+$abc33 = " ";
+$abc34 = " ";
+$abc35 = " ";
+$abc36 = " ";
+$abc37 = " ";
+$abc38 = " ";
+$abc39 = " ";
+$abc40 = " ";
+$abc41 = " ";
+$abc42 = " ";
+$abc43 = " ";
+$abc44 = " ";
+$abc45 = " ";
+$abc46 = " ";
+$abc47 = " ";
+$abc48 = " ";
+$abc49 = " ";
+$abc50 = " ";
+$abc51 = " ";
+$abc52 = " ";
+
+
+if (isset($_POST['submit'])) {
+    $abc = $_POST['value'];
+    echo "Value is : $abc";
+    echo "<br />";
+    for ($a = 0; isset($abc[$a]); $a++) {
+        if ($abc[$a] === 'A') {
+            $abc1 = 'a';
+        } elseif ($abc[$a] === 'B') {
+            $abc2 = 'b';
+        } elseif ($abc[$a] === 'C') {
+            $abc3 = 'c';
+        } elseif ($abc[$a] === 'D') {
+            $abc4 = 'd';
+        } elseif ($abc[$a] === 'E') {
+            $abc5 = 'e';
+        } elseif ($abc[$a] === 'F') {
+            $abc6 = 'f';
+        } elseif ($abc[$a] === 'G') {
+            $abc7 = 'g';
+        } elseif ($abc[$a] === 'H') {
+            $abc8 = 'h';
+        } elseif ($abc[$a] === 'I') {
+            $abc9 = 'i';
+        } elseif ($abc[$a] === 'J') {
+            $abc10 = 'j';
+        } elseif ($abc[$a] === 'K') {
+            $abc11 = 'k';
+        } elseif ($abc[$a] === 'L') {
+            $abc12 = 'l';
+        } elseif ($abc[$a] === 'M') {
+            $abc13 = 'm';
+        } elseif ($abc[$a] === 'N') {
+            $abc14 = 'n';
+        } elseif ($abc[$a] === 'O') {
+            $abc15 = 'o';
+        } elseif ($abc[$a] === 'P') {
+            $abc16 = 'p';
+        } elseif ($abc[$a] === 'Q') {
+            $abc17 = 'q';
+        } elseif ($abc[$a] === 'R') {
+            $abc18 = 'r';
+        } elseif ($abc[$a] === 'S') {
+            $abc19 = 's';
+        } elseif ($abc[$a] === 'T') {
+            $abc20 = 't';
+        } elseif ($abc[$a] === 'U') {
+            $abc21 = 'u';
+        } elseif ($abc[$a] === 'V') {
+            $abc22 = 'v';
+        } elseif ($abc[$a] === 'W') {
+            $abc23 = 'w';
+        } elseif ($abc[$a] === 'X') {
+            $abc24 = 'x';
+        } elseif ($abc[$a] === 'Y') {
+            $abc25 = 'y';
+        } elseif ($abc[$a] === 'Z') {
+            $abc26 = 'z';
+        }
+        ///////////////////////////////////////////////////////////////
+        elseif ($abc[$a] === 'a') {
+            $abc27 = 'A';
+        } elseif ($abc[$a] === 'b') {
+            $abc28 = 'B';
+        } elseif ($abc[$a] === 'c') {
+            $abc29 = 'C';
+        } elseif ($abc[$a] === 'd') {
+            $abc30 = 'D';
+        } elseif ($abc[$a] === 'e') {
+            $abc31 = 'E';
+        } elseif ($abc[$a] === 'f') {
+            $abc32 = 'F';
+        } elseif ($abc[$a] === 'g') {
+            $abc33 = 'G';
+        } elseif ($abc[$a] === 'h') {
+            $abc34 = 'H';
+        } elseif ($abc[$a] === 'i') {
+            $abc35 = 'I';
+        } elseif ($abc[$a] === 'j') {
+            $abc36 = 'J';
+        } elseif ($abc[$a] === 'k') {
+            $abc37 = 'K';
+        } elseif ($abc[$a] === 'l') {
+            $abc38 = 'L';
+        } elseif ($abc[$a] === 'm') {
+            $abc39 = 'M';
+        } elseif ($abc[$a] === 'n') {
+            $abc40 = 'N';
+        } elseif ($abc[$a] === 'o') {
+            $abc41 = 'O';
+        } elseif ($abc[$a] === 'p') {
+            $abc42 = 'P';
+        } elseif ($abc[$a] === 'q') {
+            $abc43 = 'Q';
+        } elseif ($abc[$a] === 'r') {
+            $abc44 = 'R';
+        } elseif ($abc[$a] === 's') {
+            $abc45 = 'S';
+        } elseif ($abc[$a] === 't') {
+            $abc46 = 'T';
+        } elseif ($abc[$a] === 'u') {
+            $abc47 = 'U';
+        } elseif ($abc[$a] === 'v') {
+            $abc48 = 'V';
+        } elseif ($abc[$a] === 'w') {
+            $abc49 = 'W';
+        } elseif ($abc[$a] === 'x') {
+            $abc50 = 'X';
+        } elseif ($abc[$a] === 'y') {
+            $abc51 = 'Y';
+        } elseif ($abc[$a] === 'z') {
+            $abc52 = 'Z';
+        }
+    }
+    echo "$abc1" . "$abc2" . "$abc3" . "$abc4" . "$abc5" . "$abc6" . "$abc7" . "$abc8" . "$abc9" . "$abc10" . "$abc11" . "$abc12" . "$abc13" . "$abc14" . "$abc15" . "$abc16" . "$abc17" . "$abc18" . "$abc19" . "$abc20" . "$abc21" . "$abc22" . "$abc23" . "$abc24" . "$abc25" . "$abc26" . "$abc27" . "$abc28" . "$abc29" . "$abc30" . "$abc31" . "$abc32" . "$abc33" . "$abc34" . "$abc35" . "$abc36" . "$abc37" . "$abc38" . "$abc39" . "$abc40" . "$abc41" . "$abc42" . "$abc43" . "$abc44" . "$abc45" . "$abc46" . "$abc47" . "$abc48" . "$abc49" . "$abc50" . "$abc51" . "$abc52";
+
+    echo "<br />";
+}
+?>
