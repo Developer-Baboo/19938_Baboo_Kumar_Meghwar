e3759493d1876512ed9bb83bfe1ed9b4f7c5d37c
Developer-Baboo <babooheerani@gmail.com>
2023-02-25 23:34:54 +0500

    updated..

--- Topic 7 String Manipulations/day-02/3 REVERSE A STRING (EVENING)/19938_Baboo_Kumar_Meghwar.php
+++ Topic 7 String Manipulations/day-02/3 REVERSE A STRING (EVENING)/19938_Baboo_Kumar_Meghwar.php
@@ -2,38 +2,57 @@
 <html>
 
 <head>
-    <title>Total VOWELS, CONSONANTS & POSITIONS OF VOWELS</title>
+    <title>Reverse String</title>
     <style>
-        * {
-            margin: 0px;
-            padding: 0px;
+        p {
+            color: black;
+            font-weight: bold;
+            background-color: lightcoral;
+            font-size: 20px;
+            border: 2px solid red;
+            padding: 20px;
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+            margin-left: 300px;
+            margin-right: 300px;
         }
     </style>
 </head>
 
 <body>
+
     <center>
+        <p>Reverse String</p>
+        <!-- <br> -->
+        <hr>
         <?php
         if (isset($_POST['submit'])) {
-            $value = $_POST['value'];
-            for ($a = 0; isset($value[$a]); $a++) 
-            {
-                
+            $str = $_POST['value'];
+            $len = 0;
+            while (isset($str[$len])) {
+                $len++;
             }
+            $new_str = "";
+            $i = $len - 1;
+            while ($i >= 0) {
+                $new_str .= $str[$i];
+                $i--;
+            }
+        ?>
+            <center>
+                <?php
+                echo "Input : $str";
+                echo "<br />";
+                echo "Output : $new_str";
+                ?>
+            </center>
+        <?php
         }
         ?>
-    </center>
-    <center>
-        <p style="text-align:center; color:white; background-color: palevioletred; padding: 10px;">REVERSE A STRING (EVENING) </p>
-
-
-        <br>
-        <br>
-        <form action="" method="POST">
+        <form method="POST">
             <table>
                 <tr>
-                    <td><input type="text" name="string"> </td>
-                    <td> <input type="submit" name="reverse" value="reverse"> </td>
+                    <td><input type="text" name="value"> </td>
+                    <td><input type="submit" value="submit" name="submit"></td>
                 </tr>
             </table>
         </form>
