f5bec3921a0e30dae86aa66cc5ba50de1b41f373
Developer-Baboo <babooheerani@gmail.com>
2023-03-01 10:26:40 +0500

    updated......

--- TOPIC 8 Array/day_02/LINEAR SEARCH ON AN ARRAY (EVENING)/19938_Baboo_Kumar_Meghwar.php
+++ TOPIC 8 Array/day_02/LINEAR SEARCH ON AN ARRAY (EVENING)/19938_Baboo_Kumar_Meghwar.php
@@ -1,9 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+        .button {
+            background-color: lightgreen;
+            /* Green */
+            border: 2 solid black;
+            color: white;
+            /* padding: 15px 32px; */
+            text-align: center;
+            font-size: 16px;
+            cursor: pointer;
+            border-radius: 10px;
+        }
+
+        input[type=number] {
+            border: 2px solid red;
+            border-radius: 4px;
+        }
+
+        center {
+            color: green;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <form method="POST">
+            <table>
+                <tr>
+                    <td>
+                        <input type="number" name="number" value="number" required placeholder="Enter Number">
+                    </td>
+                    <td>
+                        <input class="button" type="submit" value="submit" name="submit">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+
+</html>
 <?php
-$find_item = 6;
-$first_array = [1, 3, 5, 4, 6, 7, 6];
-for ($j = 0; isset($first_array[$j]); $j++) {
-    if ($first_array[$j] === $find_item) {
-        echo "value $find_item found on  $first_array[$j]";
-        echo "<br />";
+if (isset($_POST['submit'])) {
+    $number = $_POST['number'];
+
+    $count = 0;
+    $index = 0;
+
+    $array = [1, 3, 5, 4, 6, 7, 6];
+    for ($i = 0; isset($array[$i]); $i++) {
+        if ($array[$i] == $number) {
+            $index = $i;
+?>
+            <center>
+                <?php
+                echo "value $number found on position  $index";
+                echo "<br />";
+                ?>
+            </center>
+        <?php
+            $count = $count + 1;
+        }
+    }
+    if ($count == 0) {
+        ?>
+        <center>
+            <?php
+            echo "<br/>" . "Value not found";
+            ?>
+        </center>
+<?php
     }
 }
+?>
