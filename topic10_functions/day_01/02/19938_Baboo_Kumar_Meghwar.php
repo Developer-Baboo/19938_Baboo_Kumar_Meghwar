a093644166204e13452e9500bbfe7d92f35ca575
Developer-Baboo <babooheerani@gmail.com>
2023-03-08 08:14:46 +0500

    updated..

--- topic10_functions/day_01/02/19938_Baboo_Kumar_Meghwar.php
+++ topic10_functions/day_01/02/19938_Baboo_Kumar_Meghwar.php
@@ -1,6 +1,6 @@
 <?php
-echo "i. Make user defined function of the following like built in function: <br />";
-echo " a) .strtoupper";
+echo " ***************************** i) Make user defined function of the following like built in function**************************** <br /> <br />";
+echo " a) Strtoupper";
  function new_function($my_string = "Baboo Kumar Meghwar") 
  {
     echo "<br/> Input : $my_string";
@@ -18,4 +18,39 @@ echo " a) .strtoupper";
     return $output;
 }
 echo"<br/> Output: ". new_function();
+
+echo "<br /><br/>";
+
+echo " b) Implode <br/>";
+
+echo 'Input : $arr = array("Hello", "World!", "Beautiful", "Day!") <br/> ';
+
+echo "Output : ";
+function my_function($arr = array('Hello', 'World!', 'Beautiful', 'Day!')){
+    for ($a = 0; isset($arr[$a]); $a++) {
+        echo "  $arr[$a]  ";
+    }
+}
+my_function();
+
+echo "<br /> <br /> c) Explode <br/>";
+function _my_function($str){
+    $array = [];
+    $current_str = '';
+    for ($i = 0; isset($str[$i]); $i++) {
+        if ($str[$i] == " ") {
+            $array[] = $current_str;
+            $current_str = '';
+        } else {
+            $current_str .= $str[$i];
+        }
+    }
+    $array[] = $current_str;
+    echo "<br/> Output : <br/?";
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+_my_function("19938 Baboo Kumar Meghwar");
+// $str = "19938 Baboo Kumar Meghwar";
 ?>
