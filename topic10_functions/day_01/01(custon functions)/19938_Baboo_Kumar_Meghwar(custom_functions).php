9d38fcd02ff1f3cf47a9efa414ac45dec72b406b
Developer-Baboo <babooheerani@gmail.com>
2023-03-08 11:57:12 +0500

    Finally done with all tasks of functions

--- topic10_functions/day_01/01(custon functions)/19938_Baboo_Kumar_Meghwar(custom_functions).php
+++ topic10_functions/day_01/01(custon functions)/19938_Baboo_Kumar_Meghwar(custom_functions).php
@@ -1,54 +1,110 @@
-<?php
-echo "i) Convert the following assignments into functions: <br/>";
-echo "a) Sorting <br/>";
-echo "b) Calculator In Switch <br/>";
-echo "c) Marksheet <br/>";
-echo "d) Array Sum <br/>";
-echo "Note: Make all functions in a single page only <br/>";
-echo "a) Sorting <br/>";
+<!Doctype html>
+<html>
 
-function custom_function($arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1000, 200, 10, 20])
-{
-    $optional_variable = 0;
-    $b = 0;
-    echo 'Input: $arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1000, 200, 10, 20] <br />  ';
-    echo "Output: ";
-    for ($a = 1; isset($arr[$a]); $a++) {
-        $optional_variable = $arr[$a];
-        $b = $a;
-        while ($b > 0 && $arr[$b - 1] > $optional_variable) {
-            $arr[$b] = $arr[$b - 1];
-            $b = $b - 1;
+<head>
+    <title>Custom Function</title>
+</head>
+
+<body style="background-color: lightgreen; font-weight: bold; font-family: cursive; color:blue;font-size:large; border: 4px solid red; border-radius: 20px; padding: 30px 0px 20px 50px;">
+    <?php
+
+    echo "i) Convert the following assignments into functions: <br/>";
+    echo "a) Sorting <br/>";
+    echo "b) Calculator In Switch <br/>";
+    echo "c) Marksheet <br/>";
+    echo "d) Array Sum <br/>";
+    echo "Note: Make all functions in a single page only <br/>";
+    echo "a) Sorting <br/>";
+
+    function custom_function($arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1000, 200, 10, 20])
+    {
+        $optional_variable = 0;
+        $b = 0;
+        echo 'Input: $arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1000, 200, 10, 20] <br />  ';
+        echo "Output: ";
+        for ($a = 1; isset($arr[$a]); $a++) {
+            $optional_variable = $arr[$a];
+            $b = $a;
+            while ($b > 0 && $arr[$b - 1] > $optional_variable) {
+                $arr[$b] = $arr[$b - 1];
+                $b = $b - 1;
+            }
+            $arr[$b] = $optional_variable;
         }
-        $arr[$b] = $optional_variable;
+        for ($a = 0; isset($arr[$a]); $a++) {
+            echo "$arr[$a]" . "    ";
+        }
     }
+    custom_function();
+
+    echo "<br/> <br/> b) Calculator In Switch <br/>";
+    function cal_function($num1, $num2, $operator)
+    {
+        switch (true) {
+            case ($operator === '+'):
+                $sum = $num1 + $num2;
+                echo "Addition of $num1 and $num2 is: $sum";
+                break;
+            case ($operator === '-'):
+                $substract = $num1 - $num2;
+                echo "Subtraction of $num1 and $num2 is: $substract";
+                break;
+            case ($operator === '*'):
+                $Multiply = $num1 * $num2;
+                echo "Multiplication of $num1 and $num2 is: $Multiply";
+                break;
+            case ($operator === '/'):
+                $Divide = $num1 / $num2;
+                echo "Division of $num1 and $num2  is: $Divide";
+                break;
+        }
+    }
+    cal_function(22, 11, '/');
+
+    echo "<br/> <br/> c) Marksheet In Switch <br/>";
+    function mark_function($sub1, $sub2, $sub3, $sub4, $sub5)
+    {
+        $total = $sub1 + $sub2 + $sub3 + $sub4 + $sub5;
+        echo "Your Total Marks are 500";
+        echo "<br />";
+        echo "Your obtained marks are : $total";
+        $percentage = $total * 100 / 500;
+        echo "<br />";
+        echo "Your percentage is : $percentage";
+        echo "<br />";
+        switch (true) {
+            case ($sub1 < 40 || $sub2 < 40 || $sub3 < 40 || $sub4 < 40 || $sub5 < 40):
+                echo "Sorry! You are Failed.";
+                break;
+            case ($percentage >= 40 && $percentage <= 60):
+                echo "Your grade is C";
+                echo "<br />";
+                break;
+            case ($percentage >= 61 && $percentage <= 79):
+                echo "Your grade is B";
+                echo "<br />";
+                break;
+            case ($percentage >= 80 && $percentage <= 89):
+                echo "Your grade is A";
+                echo "<br />";
+                break;
+            case ($percentage >= 90 && $percentage <= 100):
+                echo "Your grade is A+";
+                echo "<br />";
+                break;
+        }
+    }
+    mark_function(60, 70, 90, 30, 45);
+
+    echo "<br/> <br/> d) Array sum:";
+    $arr = [1, 2, 3, 4, 5];
+    echo '<br /> Input: $arr = [1, 2, 3, 4, 5];';
+    $sum = 0;
     for ($a = 0; isset($arr[$a]); $a++) {
-        echo "$arr[$a]" . "    ";
+        $sum = $sum + $arr[$a];
     }
-}
-custom_function();
+    echo "<br /> Sum:" . $sum;
+    ?>
+</body>
 
-echo "<br/> <br/> b) Calculator In Switch <br/>";
-function cal_function($num1, $num2,$operator){
-    switch (true) {
-        case ($operator === '+'):
-            $sum = $num1 + $num2;
-            echo "Addition of $num1 and $num2 is: $sum";
-            break;
-        case ($operator === '-'):
-            $substract = $num1 - $num2;
-            echo "Subtraction of $num1 and $num2 is: $substract";
-            break;
-        case ($operator === '*'):
-            $Multiply = $num1 * $num2;
-            echo "Multiplication of $num1 and $num2 is: $Multiply";
-            break;
-        case ($operator === '/'):
-            $Divide = $num1 / $num2;
-            echo "Division of $num1 and $num2  is: $Divide";
-            break;
-    }
-}
-cal_function(22, 11, '/');
-
-?>
+</html>
--- topic10_functions/day_01/02/19938_Baboo_Kumar_Meghwar.php
+++ topic10_functions/day_01/02/19938_Baboo_Kumar_Meghwar.php
@@ -1,98 +1,113 @@
-<?php
-echo " ***************************** i) Make user defined function of the following like built in function**************************** <br /> <br />";
-echo " a) Strtoupper";
- function new_function($my_string = "Baboo Kumar Meghwar") 
- {
-    echo "<br/> Input : $my_string";
-    $output = "";
-    $a = 0;
-    do{
-        $value = ord($my_string[$a]);
-        if($value >= 97 && $value <= 122) 
-        {
-            $value = $value-32;
-        }
-        $output = $output.chr($value);
-        $a++;
-    }while(isset($my_string[$a]));
-    return $output;
-}
-echo"<br/> Output: ". new_function();
+<!DOCTYPE html>
+<html>
 
-echo "<br /><br/>";
+<head>
+    <title>CUSTOM FUNCTIONS (EVENING)</title>
+</head>
 
-echo " b) Implode <br/>";
-
-echo 'Input : $arr = array("Hello", "World!", "Beautiful", "Day!") <br/> ';
-
-echo "Output : ";
-function my_function($arr = array('Hello', 'World!', 'Beautiful', 'Day!')){
-    for ($a = 0; isset($arr[$a]); $a++) {
-        echo "  $arr[$a]  ";
+<body style="background-color: black; font-weight: bold; font-family: serif; color:white;font-size:20px; border: 4px solid red; border-radius: 20px; padding: 30px 0px 20px 50px;">
+    <?php
+    echo " ***************************** i) Make user defined function of the following like built in function*************************************** <br /> <br />";
+    echo " a) Strtoupper";
+    function new_function($my_string = "Baboo Kumar Meghwar")
+    {
+        echo "<br/> Input : $my_string";
+        $output = "";
+        $a = 0;
+        do {
+            $value = ord($my_string[$a]);
+            if ($value >= 97 && $value <= 122) {
+                $value = $value - 32;
+            }
+            $output = $output . chr($value);
+            $a++;
+        } while (isset($my_string[$a]));
+        return $output;
     }
-}
-my_function();
+    echo "<br/> Output: " . new_function();
 
-echo "<br /> <br /> c) Explode <br/>";
-// echo " <br/> Input :  ";
-function _my_function($str){
-    $array = [];
-    $current_str = '';
-    echo "Input: $str <br/>";
-    for ($i = 0; isset($str[$i]); $i++) {
-        if ($str[$i] == " ") {
-            $array[] = $current_str;
-            $current_str = '';
-        } else {
-            $current_str .= $str[$i];
-        }
-    }
-    $array[] = $current_str;
-    echo "<br/> Output : <br/?";
-    echo "<pre>";
-    print_r($array);
-    echo "</pre>";
-}
-_my_function("19938 Baboo Kumar Meghwar");
-// $str = "19938 Baboo Kumar Meghwar";
+    echo "<br /><br/>";
+    echo "***************************************************************************************************************************** <br/>";
+    echo " b) Implode <br/>";
 
+    echo 'Input : $arr = array("Hello", "World!", "Beautiful", "Day!") <br/> ';
 
-echo "d) Strlen()<br/>";
-function _function_($str)
-{
-    echo "Input: $str <br/>";
-    $count = 1;
-    for ($a = 1; isset($str[$a]); $a++) {
-        $count++;
-    }
-    echo "Output: $count";
-}
-_function_("University of Sindh Jamshoro");
-
-echo "<br/>d) Strrev()<br/>";
-function rev_function($str)
-{
-    for ($a = 0; isset($str[$a]); $a++) {
-    }
-}
-rev_function("Baboo");
-
-echo "<br/>d) Range()<br/>";
-
-function range_function($start, $end)
-{
-    // $start, $end
-    echo "Starting value: $start, Ending value: $end <br/>";
     echo "Output : ";
-    for ($a = $start; $a <= $end; $a++) {
-        echo " $a ";
+    function my_function($arr = array('Hello', 'World!', 'Beautiful', 'Day!'))
+    {
+        for ($a = 0; isset($arr[$a]); $a++) {
+            echo "  $arr[$a]  ";
+        }
     }
-}
-range_function(1, 10);
+    my_function();
+    echo "<br/><br/> ***************************************************************************************************************************** <br/>";
+    echo "<br /> c) Explode <br/>";
+    function _my_function($str)
+    {
+        $array = [];
+        $current_str = '';
+        echo "Input: $str";
+        for ($i = 0; isset($str[$i]); $i++) {
+            if ($str[$i] == " ") {
+                $array[] = $current_str;
+                $current_str = '';
+            } else {
+                $current_str .= $str[$i];
+            }
+        }
+        $array[] = $current_str;
+        echo "<br/> Output :";
+        echo "<pre>";
+        print_r($array);
+        echo "</pre>";
+    }
+    _my_function("19938 Baboo Kumar Meghwar");
+    // $str = "19938 Baboo Kumar Meghwar";
+
+    echo "***************************************************************************************************************************** <br/>";
+    echo "d) Strlen()<br/>";
+    function _function_($str)
+    {
+        echo "Input: $str <br/>";
+        $count = 1;
+        for ($a = 1; isset($str[$a]); $a++) {
+            $count++;
+        }
+        echo "Output: $count";
+    }
+    _function_("University of Sindh Jamshoro");
+    echo "<br/><br/>***************************************************************************************************************************** <br/>";
+
+    echo "<br/>d) Strrev()<br/>";
+
+    function _strrev($str = "Baboo Kumar Meghwar From Tharparkar")
+    {
+        echo "Input: $str <br/>";
+        $leng = strlen($str);
+        $rev = "";
+        for ($a = $leng - 1; $a >= 0; $a--) {
+            $rev = $rev . $str[$a];
+        }
+        echo "Output:" . $rev;
+    }
+    // echo "Output: _strrev()";
+    _strrev();
+    echo "<br/><br/>***************************************************************************************************************************** <br/>";
 
 
+    echo "<br/>d) Range()<br/>";
 
+    function range_function($start, $end)
+    {
+        // $start, $end
+        echo "Starting value: $start, Ending value: $end <br/>";
+        echo "Output : ";
+        for ($a = $start; $a <= $end; $a++) {
+            echo " $a ";
+        }
+    }
+    range_function(1, 10);
+    ?>
+</body>
 
-
-
-?>
+</html>
