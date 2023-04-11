f73aa9a3a267200444a26c1d7bab3bc7998cd562
Developer-Baboo <babooheerani@gmail.com>
2023-04-11 18:25:06 +0500

    made UI

--- Topic 18 Ajax/day_02/Search_Google/19938_Baboo_Kumar_Meghwar.php
+++ Topic 18 Ajax/day_02/Search_Google/19938_Baboo_Kumar_Meghwar.php
@@ -1,3 +1,29 @@
-<?php
-echo "tesing";
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Search Like Google</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+</head>
+
+<body>
+    <center>
+        <div class="google-logo">
+            <span>G</span>
+            <span>o</span>
+            <span>o</span>
+            <span>g</span>
+            <span>l</span>
+            <span id="e">e</span>
+        </div>
+    </center>
+    <div style="margin-top:50px" class="search-box">
+        <button><i class="fa fa-search"></i></button>
+        <input type="text" placeholder="Search Google or type URL">
+        <button><i class="fa fa-times"></i></button>
+        <i class="fa fa-microphone"></i></button>
+    </div>
+</body>
+</html>
