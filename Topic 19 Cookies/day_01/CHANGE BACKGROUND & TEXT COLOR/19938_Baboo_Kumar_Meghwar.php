3af5107f7ced3235c2c3ee12462a2de20fd92dee
Developer-Baboo <babooheerani@gmail.com>
2023-04-13 17:26:24 +0500

    done with first assignment

--- Topic 19 Cookies/day_01/CHANGE BACKGROUND & TEXT COLOR/19938_Baboo_Kumar_Meghwar.php
+++ Topic 19 Cookies/day_01/CHANGE BACKGROUND & TEXT COLOR/19938_Baboo_Kumar_Meghwar.php
@@ -1,3 +1,24 @@
 <?php
-echo "tesing";
+setcookie("username","hello");
+if(isset($_COOKIE['username']))
+	{
+		?>
+			<!DOCTYPE html>
+				<html>
+				<head>
+					<meta charset="utf-8">
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<title></title>
+				</head>
+				<body style="background-color:lightgreen;" >
+
+				</body>
+				</html>
+			<p style="color:red" >Hello World</p>
+		<?php
+	}
+	else
+	{
+		echo "Hello World";
+	}
 ?>
