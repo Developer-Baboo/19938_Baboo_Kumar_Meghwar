218ae3108836062bcee407d2512dea441b9b8f79
Developer-Baboo <babooheerani@gmail.com>
2023-04-21 13:30:46 +0500

    first commit

---
+++ Topic 24 Reporting/day_02/CV_PDF/cv_form.php
@@ -0,0 +1,15 @@
+<?php
+echo "tesing";
+?>
+
+<!Doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>Assignment</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>
+
