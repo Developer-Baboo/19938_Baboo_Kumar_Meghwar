002736efcad1a6d076f163a28393666a606ab542
Developer-Baboo <babooheerani@gmail.com>
2023-04-24 01:56:03 +0500

    updated

--- crud_html/updatedata.php
+++ crud_html/updatedata.php
@@ -1,4 +1,5 @@
 <?php
+$stu_id = $_POST['sid'];
 $stu_name = $_POST['sname'];
 $stu_address = $_POST['saddress'];
 $stu_class = $_POST['sclass'];
@@ -7,7 +8,7 @@ $stu_phone = $_POST['sphone'];
 
  $conn = mysqli_connect("localhost","root","","crud") or die("Connection Failed"); 
 
-    $sql = "INSERT INTO student_01(sname, saddress,sclass, sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}') ";
+    $sql = "UPDATE student_01 SET sname = '{$stu_name}',saddress = '{$stu_address}',sclass = '{$stu_class}',sphone = '{$stu_phone}' WHERE sid = {$stu_id}";
     
     $result = mysqli_query($conn, $sql);
 
