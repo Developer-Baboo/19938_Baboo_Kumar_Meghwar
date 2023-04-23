06dc402eb6db9637bb58d784b239ae0bf05fc4fb
Developer-Baboo <babooheerani@gmail.com>
2023-04-23 23:05:57 +0500

    curd application

---
+++ crud_html/index.php
@@ -0,0 +1,53 @@
+<?php
+include 'header.php';
+?>
+<div id="main-content">
+    <h2>All Records</h2>
+    <?php
+
+    $conn = mysqli_connect("localhost","root","","crud") or die("Connection Failed"); 
+
+    $sql = "SELECT * FROM student_01 JOIN student_class ON student_01.sclass = student_class.cid";
+    
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) > 0){
+    ?>
+    <table cellpadding="7px">
+        <thead>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Class</th>
+        <th>Phone</th>
+        <th>Action</th>
+        </thead>
+        <tbody>
+            <?php
+
+            while($row = mysqli_fetch_assoc($result)){
+
+            ?>
+            <tr>
+                <td> <?php echo $row['sid']  ?> </td>
+                <td><?php echo $row['sname'] ?></td>
+                <td><?php echo $row['saddress'] ?></td>
+                <td><?php echo $row['cname'] ?></td>
+                <td><?php echo $row['sphone'] ?></td>
+                <td>
+                    <a href='edit.php'>Edit</a>
+                    <a href='delete-inline.php'>Delete</a>
+                </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+<?php } else{
+    echo "<h1 style = 'color:red; text-align: center'>Sorry ! No Record Found<h1>";
+} 
+mysqli_close($conn);
+ ?>
+</div>
+</div>
+</body>
+</html>
