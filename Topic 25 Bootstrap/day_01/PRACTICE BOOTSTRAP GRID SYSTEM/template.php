bd962a212dd35750e48aa58412e735dd9c29e056
Developer-Baboo <babooheerani@gmail.com>
2023-04-28 01:19:58 +0500

    commented

---
+++ Topic 25 Bootstrap/day_01/PRACTICE BOOTSTRAP GRID SYSTEM/template.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>My Portfilio</title>
+
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+</head>
+
+<body>
+  <br>
+  <div class="container">
+    <h1 class="fs-1 py-3 mb-2 bg-info text-white text-center rounded-4">1 Column Layout </h1>
+  </div>
+  <br>
+  <!-- <br> -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <img src="./images/tech.jpg" class="img-fluid" alt="...">
+      </div>
+    </div>
+  </div>
+  <br>
+  <!-- 2 column layout -->
+  <div class="container">
+    <h1 class="fs-1 py-3 mb-2 bg-success text-white text-center rounded-4">2 Column Layout </h1>
+  </div>
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <img src="./images/B-Tech-Degree.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card 1</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae commodi sit magnam, tempore accusamus maxime qui non mollitia. Iure distinctio sit eos consectetur, maxime delectus tempora eius placeat voluptatem animi.</p>
+            <center>
+              <button type="button" class="btn btn-outline-info">Read More</button>
+            </center>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <img src="./images/c.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card 2</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quasi maxime ducimus maiores error unde, quibusdam nostrum. Magnam quidem, quasi ab tenetur debitis modi facilis, reiciendis voluptates quia ullam sit.</p>
+            <center>
+              <button type="button" class="btn btn-outline-info">Read More</button>
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- 2 column layout end here -->
+
+  <!-- 3 Column Layout -->
+  <br>
+  <div class="container">
+    <h1 class="fs-1 py-3 mb-2 bg-dark-subtle text-white text-center rounded-4">3 Column Layout </h1>
+  </div>
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card">
+          <img src="./images/B-Tech-Degree.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card 1</h5>
+            <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, laboriosam voluptate aliquam accusantium rem dignissimos. Eius tempore totam beatae atque. Consequatur doloremque nostrum quas! Eligendi iusto corporis odio libero non.</p>
+            <center>
+              <button type="button" class="btn btn-outline-secondary">Read More</button>
+            </center>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="./images/c.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card 2</h5>
+            <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores nobis id optio temporibus corrupti eaque corporis. Omnis iure perferendis illum vel ut, voluptate illo similique esse aliquam delectus placeat nam!</p>
+            <center><button type="button" class="btn btn-outline-secondary">Read More</button></center>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="./images/comp.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card 3</h5>
+            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod eveniet ipsam vel blanditiis voluptate aliquam eius repellat autem, soluta fugit tempore magnam nostrum sit! Voluptatum tempora dolore quam maxime temporibus.</p>
+            <center><button type="button" class="btn btn-outline-secondary">Read More</button></center>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- 3 Column Layout -->
+  <!-- 4 Column Layout -->
+  <br>
+  <div class="container">
+    <h1 class="fs-1 py-3 mb-2 bg-dark-subtle text-white text-center rounded-4">4 Column Layout </h1>
+  </div>
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="card">
+          <img src="./images/B-Tech-Degree.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card 1</h5>
+            <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, laboriosam voluptate aliquam accusantium rem dignissimos. Eius tempore totam beatae atque. Consequatur doloremque nostrum quas! Eligendi iusto corporis odio libero non.</p>
+            <center>
+              <button type="button" class="btn btn-warning">Read More</button>
+            </center>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="./images/c.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card 2</h5>
+            <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores nobis id optio temporibus corrupti eaque corporis. Omnis iure perferendis illum vel ut, voluptate illo similique esse aliquam delectus placeat nam!</p>
+            <center><button type="button" class="btn btn-warning">Read More</button></center>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="./images/comp.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card 3</h5>
+            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod eveniet ipsam vel blanditiis voluptate aliquam eius repellat autem, soluta fugit tempore magnam nostrum sit! Voluptatum tempora dolore quam maxime temporibus.</p>
+            <center><button type="button" class="btn btn-warning">Read More</button></center>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="./images/comp.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card 4</h5>
+            <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod eveniet ipsam vel blanditiis voluptate aliquam eius repellat autem, soluta fugit tempore magnam nostrum sit! Voluptatum tempora dolore quam maxime temporibus.</p>
+            <center><button type="button" class="btn btn-warning">Read More</button></center>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- 4 Column Layout -->
+
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- Topic 25 Bootstrap/day_01/RESPONSIVE BOOTSTRAP WEB TEMPLATE/my_porfolio.php
+++ Topic 25 Bootstrap/day_01/RESPONSIVE BOOTSTRAP WEB TEMPLATE/my_porfolio.php
@@ -11,14 +11,12 @@
 </head>
 
 <body>
+  <!-- Navbar -Start -->
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <!-- <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button> -->
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <img src="./Images/logo1.PNG" alt="" class="me-auto">
+      <div class="navbar-collapse position-sticky ms-auto" id=" navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
@@ -29,10 +27,10 @@
             <a class="nav-link active" aria-current="page" href="#">Skills</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Portfilio</a>
+            <a class="nav-link active" aria-current="page" href="#">Portfolio</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Achivement</a>
+            <a class="nav-link active" aria-current="page" href="#">Achievement</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Contact</a>
@@ -41,8 +39,158 @@
       </div>
     </div>
   </nav>
-  
-  <img src="..." class="img-fluid" alt="...">
+  <br>
+  <br>
+  <!-- Navbar -End -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+        <h1 class="fs-1">Welcome, to My World</h1>
+        <p class="fs-4">I'am Baboo Kumar Graduated from University of Sindh and currently Intern @ HIST Jamshoro</p>
+      </div>
+      <div class="col-md-5">
+        <img src="./Images/web1.PNG" class="img-fluid rounded-4" alt="...">
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+
+  <!-- SERVICES SECTION -->
+  <br>
+  <div class="container">
+    <h1 class="fs-1 py-3 mb-2 bg-primary text-white text-center rounded-4">Services I Provide</h1>
+  </div>
+  <br>
+  <!-- <br> -->
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="card">
+          <img src="./Images/react.jpg" style="width: 60%;" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">I Provide Web Development Services</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card">
+          <img src="./Images/web_eeve.png" style="width: 67%;" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">I Provide React Development Services</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <!-- SERVICES SECTION -->
+
+  <!-- Skills SECTION -->
+  <div class="container">
+    <h1 class="fs-1 py-3 mb-2 bg-info text-white text-center rounded-4">MY SKILLS </h1>
+  </div>
+  <br>
+  <br>
+  <div style="margin-left: 80px; margin-right:80px" >
+    <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+      <div class="progress-bar" style="width: 100%">HTML</div>
+    </div>
+    <br />
+    <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+      <div class="progress-bar" style="width: 90%">CSS</div>
+    </div>
+    <br />
+    <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+      <div class="progress-bar" style="width: 50%">JavaScript</div>
+    </div>
+    <br />
+    <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+      <div class="progress-bar" style="width: 90%">Bootstrap</div>
+    </div>
+    <br />
+    <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+      <div class="progress-bar" style="width: 70%">PHP</div>
+    </div>
+    <br />
+    <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+      <div class="progress-bar" style="width: 80%">MYSQl</div>
+    </div>
+  </div>
+  <br />
+  <br>
+  <!-- Skills SECTION -->
+  <div class="container">
+    <h1 class="fs-1 py-3 mb-2 bg-secondary text-white text-center rounded-4">MY PROJECTS </h1>
+  </div>
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="card">
+          <img src="./Images/Port3.png" style="width: 75%;" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">I Designed Periodic Table when i was student at SMIT Hyderabad</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="card">
+          <img src="./Images/port11.png" style="width: 60%;" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">I Designed Salani Login Form when i was student at SMIT Hyderabads</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="card">
+          <img src="./Images/port2.JPG" style="width: 60%;" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">I Designed Facebook Login when i was student at SMIT Hyderabad</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <!-- My Activement secction? -->
+  <div class="container">
+    <h1 class="fs-1 py-3 mb-2 bg-secondary text-white text-center rounded-4">My Achievements </h1>
+  </div>
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="card">
+          <img src="./Images/ui.jpg" style="width: 75%;" class="card-img-top" alt="...">
+          <!-- <div class="card-body">
+            <p class="card-text">I Provide Web Development Services</p>
+          </div> -->
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="card">
+          <img src="./Images/JavaScript for absolute begineers.jpg" style="width: 78%;" class="card-img-top" alt="...">
+          <!-- <div class="card-body">
+            <p class="card-text">I Provide React Development Services</p>
+          </div> -->
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="card">
+          <img src="./Images/html5.PNG" style="width: 75%; text-align: center; " class="card-img-top" alt="...">
+          <!-- <div class="card-body">
+            <p class="card-text">I Provide React Development Services</p>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <!-- My Activement Section -->
 
   <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
